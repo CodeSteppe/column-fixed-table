4bf733570bb8c81245e0e14a022041d64f68c4a3
bruce_zhang <bruce_zhang@trendmicro.com>
2023-03-09 13:43:17 +0800

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-const tableContainer = document.querySelector('.table-container');
-const firstColumn = tableContainer.querySelectorAll('td:first-child, th:first-child');
-tableContainer.addEventListener('scroll', () => {
-  const scrollLeft = tableContainer.scrollLeft;
-  firstColumn.forEach((cell) => {
-    cell.style.transform = `translateX(${scrollLeft}px)`;
-  });
-});
